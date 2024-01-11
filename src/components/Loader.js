0404ed1f341ae6f99ce870c3715a7dccaa55c09c
leonard <you@example.com>
2024-01-11 14:47:41 +0100

    loadingscreen and searchbar

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import { CirclesWithBar, Vortex } from "react-loader-spinner";
 
 export default function Loader() {
   return (
-    <div style={{ backgroundImage: 'url("loading-dog.gif")' }}></div>
+    <div className='w-screen h-screen flex justify-center items-center'><Vortex
+      height="100"
+      width="100"
+      color="#4fa94d"
+      outerCircleColor="#4fa94d"
+      innerCircleColor="#4fa94d"
+      barColor="#4fa94d"
+      ariaLabel="circles-with-bar-loading"
+      wrapperStyle={{}}
+      wrapperClass=""
+      visible={true}
+    /></div>
   )
 }
